bd05d73e50bcf52ed2c8aa282e34aecd3c38afa0
HannaLuniova <74614685+HannaLuniova@users.noreply.github.com>
2021-03-20 14:09:55 +0300

    unionTable

--- sql/vlozenie_zapros.sql
+++ sql/vlozenie_zapros.sql
@@ -1,9 +1,12 @@
 -- находим человека с max зп
-SELECT `fio`
+SELECT `fio
+`
 FROM `vedomost`
-WHERE `zp` =(
+WHERE `zp` =
+(
 SELECT
-    MAX(`zp`)
+    MAX(`zp
+`)
 FROM
     `vedomost`
 )
@@ -11,77 +14,98 @@ FROM
 SELECT
     *
 FROM
-    `vedomost`
+    `vedomost
+`
 WHERE
-    `zp` >(
+    `zp` >
+(
 SELECT
-    AVG(`zp`)
+    AVG(`zp
+`)
 FROM
     `vedomost`
 )
 
 -- кол-во людей в каждом отделе
-SELECT `otdel`,COUNT(*)
+SELECT `otdel
+`,COUNT
+(*)
 FROM vedomost
 GROUP BY `otdel`
 
 --кол-во людей в отделе 'Бухгалтерия'
 SELECT
-    `otdel`,
-    COUNT(*)
+    `otdel
+`,
+    COUNT
+(*)
 FROM
     vedomost
 WHERE
     `otdel` = 'Бухгалтерия'
 GROUP BY
     `otdel`
-    
+
 -- выводит кол-во человек в каждом отделе, если в отделе работает два и более человека
-    SELECT
-    `otdel`,
-    COUNT(*)
+SELECT
+    `otdel
+`,
+    COUNT
+(*)
 FROM
     vedomost
 GROUP BY
     `otdel`
 HAVING
-    COUNT(*) >= 2
+    COUNT
+(*) >= 2
 
 -- средняя зп в каждом отделе
-    SELECT
-    `otdel`,
-    AVG(`zp`)
-FROM
-    vedomost
-GROUP BY
-    `otdel`
-
-    -- ищет max, min з/п
-    SELECT
-    `otdel` AS 'Отдел',
-    MIN(`zp`) AS 'Минимальная зарплата',
-    MAX(`zp`) AS 'Максимальная зарплата'
-FROM
-    vedomost
-GROUP BY
-    `otdel`
-
-    -- найти максимальную зарплату в отделах , где работает больше одного человека
 SELECT
-    `otdel` AS 'Отдел',
-    MIN(`zp`) AS 'Минимальная зарплата',
-    MAX(`zp`) AS 'Максимальная зарплата'
+    `otdel
+`,
+    AVG
+(`zp`)
+FROM
+    vedomost
+GROUP BY
+    `otdel`
+
+-- ищет max, min з/п
+SELECT
+    `otdel
+` AS 'Отдел',
+    MIN
+(`zp`) AS 'Минимальная зарплата',
+    MAX
+(`zp`) AS 'Максимальная зарплата'
+FROM
+    vedomost
+GROUP BY
+    `otdel`
+
+-- найти максимальную зарплату в отделах , где работает больше одного человека
+SELECT
+    `otdel
+` AS 'Отдел',
+    MIN
+(`zp`) AS 'Минимальная зарплата',
+    MAX
+(`zp`) AS 'Максимальная зарплата'
 FROM
     vedomost
 GROUP BY
     `otdel`
 HAVING
-    COUNT(*) >= 1
+    COUNT
+(*) >= 1
 ------------------------------Таблицы `customers`, `orders`, `salespeople`-----------------------------------------------------------------------
- --выводит кол-во клиентов в каждом городе
+--выводит кол-во клиентов в каждом городе
 SELECT
-    `CITY`,
-    COUNT(*)
+    `CITY
+`,
+    COUNT
+(*)
 FROM
     `customers`
 GROUP BY
@@ -94,86 +118,96 @@ FROM
 WHERE
     `COMM` =(
         SELECT
-            MIN(`COMM`)
+    MIN(`COMM
+`)
         FROM
             salespeople
     )
 
 -- выводит покупателя с самым высоким рейтингом
-    SELECT
+SELECT
     *
 FROM
-    `customers`
+    `customers
+`
 WHERE
-    `RATING` =(
+    `RATING` =
+(
         SELECT
-            MAX(`RATING`)
+    MAX(`RATING
+`)
         FROM
             customers
     )
 
 -- выводит клиентов с рейтингом от 150 до 250
-    SELECT
+SELECT
     *
 FROM
-    `customers`
+    `customers
+`
 WHERE
     `RATING` BETWEEN 150 AND 250
 
-    -- выводит клиентов из "London","Berlin","Rome"
-    SELECT
+-- выводит клиентов из "London","Berlin","Rome"
+SELECT
     *
 FROM
-    `customers`
+    `customers
+`
 WHERE
-    `CITY` IN ("London", "Berlin", "Rome")
+    `CITY` IN
+("London", "Berlin", "Rome")
 
 --выводит клиентов с номером 2002,2006,2008
-    SELECT
+SELECT
     *
 FROM
-    `customers`
+    `customers
+`
 WHERE
-    `CNUM` IN (2002, 2006, 2008)
+    `CNUM` IN
+(2002, 2006, 2008)
 
-    --выводит клиентов с окончанием в имени на 's'
-    SELECT
+--выводит клиентов с окончанием в имени на 's'
+SELECT
     *
 FROM
-    `customers`
-WHERE
-    `CNAME` LIKE '%s'
+    `customers
+`
+WHERE`CNAME` LIKE '%s'
 
-    --Найти продавца обслужившего самый большой заказ
-    SELECT
+--Найти продавца обслужившего самый большой заказ
+SELECT
     SNAME
 FROM
     salespeople
 WHERE
     SNUM =(
         SELECT
-            `SNUM`
+    `SNUM`
         FROM
             orders
         WHERE
-            `AMT` =(
-                SELECT
-                    MAX(`AMT`)
-                FROM
-                    orders
-            )
+            `AMT` =
+                    (SELECT MAX(`AMT`)
+                     FROM orders
+                                     )
     )
-    --Найти все заказы клиента по имени Grass
-    SELECT
+--Найти все заказы клиента по имени Grass
+SELECT
     *
 FROM
-    `orders`
+    `orders
+`
 WHERE
-    CNUM =(
+    CNUM =
+(
         SELECT
-            CNUM
-        FROM
-            `customers`
+    CNUM
+FROM
+    `customers
+`
         WHERE
             cname = "Grass"
     )
